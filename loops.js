63edadf80ff10dc67acd058b57347e3bf24f57d5
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-10 18:06:13 +0100

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,23 @@
+
+// FOR LOOPS
+
+for(let i = 0; i < 100; i++){
+
+
+    if (i === 40)  {
+        console.log('Hey you hit 40!');
+        break;
+    }
+    // RUN SOME CODE
+    console.log(i);
+
+}
+
+// use texts.length instead of amount - 
+
+const text = 'kris z'
+
+for (let i = 0; i < text.length; i++) {
+    // RUN SOME CODE
+    console.log(i);
+}
