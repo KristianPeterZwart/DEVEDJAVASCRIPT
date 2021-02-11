73f2b737a3f162e0d40f69651c0188c2e7f9bd7c
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-11 18:16:41 +0100

    Create scope.js

---
+++ scope.js
@@ -0,0 +1,20 @@
+// GLOBAL SCOPE
+    let food = 'apple'
+    let fruits = 5
+    var x = 5
+
+
+function store() {
+    // FUNCTION SCOPE
+    let food = "banana"
+    let fruits = 50
+    var x = 5
+}
+
+
+    // BLOCK SCOPE
+for (let x = 0; x < 10; x++) {
+    console.log(fruits)
+}
+
+console.log("Global" + x)
