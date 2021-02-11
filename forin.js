b86e27982eb7ddea21abaafde9064211ac6052dd
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-11 17:16:06 +0100

    Update forin.js

---
+++ forin.js
@@ -0,0 +1,37 @@
+
+
+// const user = {
+    // name: 'Kris Zwart',
+    // age: 44,
+    // subscribers: 235,
+    // money: 'lolno'
+// }
+// for(let x in user) {
+    // console.log(user[x]);
+// }
+
+// console.log(user['subscribers'])
+
+// name = key - kris zwart = value - the whole line equals property
+
+
+// WHILE, DO WHILE, DO WHILE - always add increment 
+
+// let i = 0;
+// 
+// while(i < 30){
+    // console.log(i)
+    // i+=2
+// }
+// 
+// 
+let i = 0;
+
+do{
+    console.log(i)
+    i++;
+}
+
+while(i > 10);
+
+// do will run once even if false
