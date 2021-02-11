8af4145fa7b587884ebd7a524f660470c8e7f9e7
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-11 17:56:57 +0100

    Random Number Console Game

---
+++ adventure.js
@@ -0,0 +1,21 @@
+// Generate a random user name DONE
+// give the user the ability to guess
+// if they guess and they are wrong, ask them again (hint);
+// if they win - say they have won.
+
+function guessGame() {
+    let randomNr = Math.floor(Math.random() * 11);
+    let guess;
+    do {
+        guess = prompt("Guess a number between 1-10");
+        console.log(guess, randomNr);
+        if(randomNr > guess) {
+            console.log("You guessed too low");
+        } else if(randomNr < guess) {
+            console.log("Guess was too high");
+        }
+    } while (guess != randomNr);
+    console.log('YOU WON')
+}
+
+guessGame();
---
+++ objects.js
@@ -0,0 +1,15 @@
+
+// const name = 'Kris'
+// const age = 25
+
+const user = {
+    name: 'Kris',
+    age: 44,
+    married: false,
+    greet: function() {this
+    console.log("Hello there!");
+    }
+}
+
+console.warn('OMG error')
+user.greet();
