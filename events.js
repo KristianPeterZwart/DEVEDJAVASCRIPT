aa1199e55bd1103541076f32aedce6d8d4d0ed5b
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-12 18:32:29 +0100

    update

--- events.js
+++ events.js
@@ -1,18 +1,22 @@
 const button = document.querySelector("#submit");
 const todoList = document.querySelector("#todo-list");
 const todoNr = document.querySelector(".todo-nr b");
-const items = todoList.children;
+const mainTitle = document.querySelector(".main-title");
 
-// CLICK, SCROLL. RESIZING THE BROWSER
+// const items = todoList.children;
+// 
+// // CLICK, SCROLL. RESIZING THE BROWSER
 
-// ATTACH A EVENT LISTENER
+// // ATTACH A EVENT LISTENER
+
+// button.addEventListener("click", function() {
+//     const newItem = document.createElement("li");
+//     newItem.classList.add("item");
+//     newItem.innerText = `item ${items.length + 1}`;
+//     todoList.appendChild(newItem);
+//     todoNr.innerText = items.length;
+// })
 
 button.addEventListener("click", function() {
-    const newItem = document.createElement("li");
-    newItem.classList.add("item");
-    newItem.innerText = "Item 3";
-    todoList.appendChild(newItem);
-    todoNr.innerText = items.length;
-})
-
-
+ mainTitle.classList.add("spectacular")
+});
