2446fe20f901df727b0ef1cdbfe0d505f67ba225
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-12 15:55:38 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,15 +1,15 @@
-// function greet() {
-    // let name = prompt("What is your name?")
-    // console.log("Welcome to our website ' + name")
-// }
-// 
-// console.log("rest of the code");
-// 
-// greet();
-// 
-// let maxNr = Math.max(1,5);
-// 
-// console.log(MaxNr)
+function greet() {
+    let name = prompt("What is your name?")
+    console.log("Welcome to our website ' + name")
+}
+
+console.log("rest of the code");
+
+greet();
+
+let maxNr = Math.max(1,5);
+
+console.log(MaxNr)
 
 function max(nr1, nr2){
     if (nr1 > nr2) {
