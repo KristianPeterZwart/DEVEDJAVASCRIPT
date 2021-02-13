05556debb39fa97802e9493aedd891f42d91c09e
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-13 17:01:14 +0100

    Update local-storage.js

--- local-storage.js
+++ local-storage.js
@@ -2,8 +2,15 @@
 // LOCAL STORAGE
 
 localStorage.setItem("todo", "Feed the dog");
+localStorage.setItem("user", "Kris");
+localStorage.setItem("todo", "Feed myself");
+
 
 // SESSION STORAGE
-
 sessionStorage.setItem("todoList", "session feeding dog");
+
+// GETTING STUFF BACK
+const user = localStorage.getItem("user");
+
+console.log(user)
 
